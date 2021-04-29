7b099790c7fdafda83a1e36ad47198408819b558
fikridzkr <fikridzakir179@gmail.com>
2021-04-29 20:59:48 +0700

    what is express

--- index.js
+++ index.js
@@ -2,8 +2,37 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// app.use("/", (req, res) => {
+//   res.send("test");
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile("./index.html", { root: __dirname });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    name: "Fikri",
+    email: "fikri@gmail.com",
+    noHP: "0812332323",
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile("./contact.html", { root: __dirname });
+});
+
+app.get("/products/:id", (req, res) => {
+  res.send(`Products ID ${req.params.id} \n Category ${req.query.category}`);
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("404");
 });
 
 app.listen(port, () => {
