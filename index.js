80ff0509ef0ec1c06cd8488201665c10f7a4e8ac
fikridzkr <fikridzakir179@gmail.com>
2021-05-02 13:40:14 +0700

    Express View Engine

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 3000;
 
-// app.use("/", (req, res) => {
-//   res.send("test");
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 app.get("/", (req, res) => {
-  res.sendFile("./index.html", { root: __dirname });
+  const siswa = [
+    {
+      nama: "Fikri",
+      email: "fik@gmail.com",
+    },
+    {
+      nama: "Ronaldo",
+      email: "cr7@gmail.com",
+    },
+    {
+      nama: "Messi",
+      email: "m10@gmail.com",
+    },
+  ];
+  res.render("index", {
+    nama: "Fikri Dzakir",
+    title: "Halaman Index",
+    siswa,
+    title: "Home Page",
+    layout: "layouts/main-layout",
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    title: "Contact Page",
+    layout: "layouts/main-layout",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Page", layout: "layouts/main-layout" });
+});
+
+app.get("/products/:id", (req, res) => {
+  res.send(`Products ID ${req.params.id} \n Category ${req.query.category}`);
 });
 
 app.get("/api", (req, res) => {
@@ -21,15 +51,6 @@ app.get("/api", (req, res) => {
     noHP: "0812332323",
   });
 });
-
-app.get("/contact", (req, res) => {
-  res.sendFile("./contact.html", { root: __dirname });
-});
-
-app.get("/products/:id", (req, res) => {
-  res.send(`Products ID ${req.params.id} \n Category ${req.query.category}`);
-});
-
 app.use("/", (req, res) => {
   res.status(404);
   res.send("404");
