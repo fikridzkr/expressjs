c5fc19449facdb2eaa881b7d022c81e762668d9a
fikridzkr <fikridzakir179@gmail.com>
2021-05-02 14:30:14 +0700

    Express MiddleWare

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
+
+// third-party middleware
+app.use(morgan("dev"));
+// built in middleware
+app.use(express.static("public"));
+
+// application level middleware
+app.use((req, res, next) => {
+  console.log("Time:", Date.now());
+  next();
+});
+
 app.get("/", (req, res) => {
   const siswa = [
     {
